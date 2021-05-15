238a5043cd77fb0a3080f5ce4572c454af0b0ea2
Asha Hakari <asha.hakari@gmail.com>
2021-05-14 22:59:52 -0700

    Completed Module

--- js/ratings.js
+++ js/ratings.js
@@ -21,5 +21,5 @@ function collect_ratings()
 
 document.addEventListener('change', () => {
     const ratings = collect_ratings();
-    document.getElementById('average').value = (ratings.average).toFixed(2);
+    document.querySelector('#average').value = (ratings.average).toFixed(2);
 })
